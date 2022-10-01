0c3e84a875c514dab4669db1b281171289135888
yoann.archier <enforetco@gmail.com>
2022-10-01 17:04:28 +0200

    Spi Functionnal, Display working

--- main.cpp
+++ main.cpp
@@ -8,19 +8,116 @@
 
 // Définitions
 #define GPIO_IRQ_FALLING_EDGE 0x04
-
+#define addCTRL 0x0F
+#define addSec 0x00
+#define addMin 0x01
+#define addHrs 0x02
+#define Secondes 2
+#define Minutes 1
+#define Heures 0
 // Definitions des pins
 #define Pin_En 22
-#define Pin_D 20
 #define Pin_CP 21
+#define Pin_D 20
 #define Pin_STR 19
-#define Pin_Oe 18
-#define BUTTON_MODE 17
+
 #define BUTTON_UP 18
+#define BUTTON_MODE 17
 #define BUTTON_DOWN 16
+
+#define Pin_CS 29
+#define Pin_1Hz 28
 #define Pin_MISO 4
 #define Pin_MOSI 3
 #define Pin_SCK 2
+
+// Variables globales
+spi_inst_t *spi = spi0;
+uint8_t tabBuf[2];
+uint8_t tabHeure[3];
+uint16_t tabDigit[4] = {0, 0, 0, 0};
+uint8_t etatClock = 1;
+uint16_t tabConvert[10]=
+        {
+                0x0001,//0
+                0x0002,//1
+                0x0004,//2
+                0x0008,//3
+                0x0010,//4
+                0x0020,//5
+                0x0040,//6
+                0x0080,//7
+                0x0100,//8
+                0x0200//9
+        };
+
+int reg_read(  spi_inst_t *spi_l,
+               const uint cs,
+               const uint8_t reg,
+               uint8_t *buf,
+               const uint8_t nbytes) {
+
+    int num_bytes_read = 0;
+    uint8_t mb = 0;
+
+    // Determine if multiple byte (MB) bit should be set
+    if (nbytes < 1) {
+        return -1;
+    } else if (nbytes == 1) {
+        mb = 0;
+    } else {
+        mb = 1;
+    }
+    // Construct message (set ~W bit high)
+    uint8_t msg = ~0x80 & (mb << 6) | reg;
+
+    // Read from register
+    gpio_put(cs, 1);
+    spi_write_blocking(spi_l, &msg, 1);
+    num_bytes_read = spi_read_blocking(spi_l, 0, buf, nbytes);
+    gpio_put(cs, 0);
+
+    return num_bytes_read;
+}
+void RegWrite(spi_inst_t *spi_l, const uint cs, const uint8_t reg, const uint8_t data)
+{
+    uint8_t msg[2];
+
+    // Construct message (set ~W bit low, MB bit low)
+    msg[0] = 0x80 | reg;
+    msg[1] = data;
+
+    // Write to register
+    gpio_put(cs, 1);
+    spi_write_blocking(spi_l, msg, 2);
+    gpio_put(cs, 0);
+}
+void AffichageTubes(uint16_t Tubes[4])
+{
+    uint16_t i,j,buf;
+    uint16_t msk;
+    gpio_put(Pin_STR, true);
+    for (j=0;j<4;j++)
+    {
+        msk = 0x0800;
+        for (i = 12; i > 0; i--)
+        {
+            gpio_put(Pin_D, !((Tubes[j] & msk) >> (i - 1))); // Output data
+            gpio_put(Pin_CP, true); // coup de clock
+            //sleep_ms(10);
+            for(buf=0;buf<64000;buf++);
+            for(buf=0;buf<64000;buf++);
+            for(buf=0;buf<64000;buf++);
+            gpio_put(Pin_CP, false);
+            for(buf=0;buf<64000;buf++);
+            for(buf=0;buf<64000;buf++);
+            for(buf=0;buf<64000;buf++);
+
+            msk = msk >> 1;
+        }
+    }
+    gpio_put(Pin_STR, false);
+}
 int64_t AlarmCallback(alarm_id_t id, void *user_data)
 {
     std::cout << "AlarmCallback" << std::endl;
@@ -28,35 +125,74 @@ int64_t AlarmCallback(alarm_id_t id, void *user_data)
     return 0;
 }
 
-void ButtonModeCallback(uint gpio, uint32_t events)
+//void ButtonModeCallback(uint gpio, uint32_t events)
+//{
+//
+//}
+void SynchroCallback(uint gpio, uint32_t events)
 {
-    std::cout << "ButtonModeCallback" << std::endl;
-    // Anti-rebond virtuelle
-    gpio_set_irq_enabled(BUTTON_MODE, GPIO_IRQ_FALLING_EDGE, false);
-    add_alarm_in_ms(500, AlarmCallback, NULL, false);
-    gpio_put(PICO_DEFAULT_LED_PIN, !gpio_get(PICO_DEFAULT_LED_PIN)); // Led
-
-    // On entre dans le mode de réglage de l'heure
-
-}
-
-
-void WriteDigit(int value)
-{
-    int i;
-    int msk = 0x0200;
-    for (i = 10; i > 0; i--)
+    if (gpio == Pin_1Hz)
     {
-        gpio_put(Pin_D, !((value & msk)>>i-1)); // Output data
-        gpio_put(Pin_CP, true); // coup de clock
-        sleep_ms(500);
-        gpio_put(Pin_CP, false);
-        sleep_ms(500);
+        std::cout << "Time 1Hz interrupt" << std::endl;
+        reg_read(spi, Pin_CS, addSec, tabBuf, 1);
+        tabHeure[Secondes] = tabBuf[0];
+        std::cout << "data: " << std::to_string(tabBuf[0]) << std::endl;
+        reg_read(spi, Pin_CS, addMin, tabBuf, 1);
+        tabHeure[Minutes] = tabBuf[0];
+        std::cout << "data: " << std::to_string(tabBuf[0]) << std::endl;
+        reg_read(spi, Pin_CS, addHrs, tabBuf, 1);
+        tabHeure[Heures] = tabBuf[0];
+        std::cout << "data: " << std::to_string(tabBuf[0]) << "\n" << std::endl;
 
-        msk = msk >> 1;
+
+        if ((tabHeure[Secondes] & 0x0F) == 0x00) {
+            std::cout << "10Secondes ! " << "\n" << std::endl;
+            //if((tabHeure[2]>>4)==0x03 || (tabHeure[2]>>4)==0x00)
+            {
+                std::cout << "Heures : " << std::to_string(tabHeure[Secondes] >> 4) << std::to_string(tabHeure[Secondes] & 0x0F) << ":"
+                          << std::to_string(tabHeure[Minutes] >> 4) << std::to_string(tabHeure[Minutes] & 0x0F) << ":"
+                          << std::to_string(tabHeure[Heures] >> 4) << std::to_string(tabHeure[Heures] & 0x0F) << "\n" << std::endl;
+
+                tabDigit[0] = tabConvert[tabHeure[Minutes] & 0x0f];
+                tabDigit[1] = tabConvert[tabHeure[Minutes] >> 4];
+                tabDigit[2] = tabConvert[tabHeure[Heures] & 0x0f];
+                tabDigit[3] = tabConvert[tabHeure[Heures] >> 4];
+                AffichageTubes(tabDigit);
+            }
+        }
+    }
+    if(gpio==BUTTON_MODE)
+    {
+        std::string str;
+        std::cout << "BUTTON_MODE Interrupt" << std::endl;
+        // Anti-rebond virtuelle
+        //gpio_set_irq_enabled(BUTTON_MODE, GPIO_IRQ_FALLING_EDGE, false);
+        //add_alarm_in_ms(500, AlarmCallback, NULL, false);
+        gpio_put(PICO_DEFAULT_LED_PIN, !gpio_get(PICO_DEFAULT_LED_PIN)); // Led
+        if(etatClock==1)
+        {
+            // On entre dans le mode de réglage de l'heure
+            etatClock = 0;
+            std::cout << "Stop clock" << std::endl;
+            RegWrite(spi, Pin_CS, addCTRL, 0x00);
+        }
+        else
+        {
+            etatClock = 1;
+            std::cout << "Run clock" << std::endl;
+            RegWrite(spi, Pin_CS, addCTRL, 0x04);
+            AffichageTubes(tabDigit);
+        }
+    }
+    if(gpio==BUTTON_UP)
+    {
+//        if(etatClock==1)
+//        {
+//            tabHeure[1]++;
+//
+//        }
     }
 }
-
 int main()
 {
     // Initialisation des pins
@@ -73,35 +209,49 @@ int main()
     gpio_init(Pin_STR);
     gpio_set_dir(Pin_STR, GPIO_OUT);
 
-    gpio_init(Pin_Oe);
-    gpio_set_dir(Pin_Oe, GPIO_OUT);
-
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
     gpio_init(BUTTON_MODE);
     gpio_set_dir(BUTTON_MODE, GPIO_IN);
     gpio_pull_up(BUTTON_MODE);
-    gpio_set_irq_enabled_with_callback(BUTTON_MODE, GPIO_IRQ_FALLING_EDGE, true, ButtonModeCallback);
+    gpio_set_irq_enabled_with_callback(BUTTON_MODE, GPIO_IRQ_FALLING_EDGE, true, SynchroCallback);
+
+    gpio_init(BUTTON_DOWN);
+    gpio_set_dir(BUTTON_DOWN, GPIO_IN);
+    gpio_pull_up(BUTTON_DOWN);
+    gpio_set_irq_enabled_with_callback(BUTTON_DOWN, GPIO_IRQ_FALLING_EDGE, true, SynchroCallback);
+
+    gpio_init(BUTTON_UP);
+    gpio_set_dir(BUTTON_UP, GPIO_IN);
+    gpio_pull_up(BUTTON_UP);
+    gpio_set_irq_enabled_with_callback(BUTTON_UP, GPIO_IRQ_FALLING_EDGE, true, SynchroCallback);
+
 
     // Initialize SPI pins
     gpio_set_function(Pin_SCK, GPIO_FUNC_SPI);
     gpio_set_function(Pin_MOSI, GPIO_FUNC_SPI);
     gpio_set_function(Pin_MISO, GPIO_FUNC_SPI);
 
+    gpio_init(Pin_CS);
+    gpio_set_dir(Pin_CS,GPIO_OUT);
+
+    gpio_init(Pin_1Hz);
+    gpio_set_dir(Pin_1Hz,GPIO_IN);
+    gpio_set_irq_enabled_with_callback(Pin_1Hz,GPIO_IRQ_EDGE_RISE, true,SynchroCallback);
+
     // Initialisation des états initials
     gpio_put(Pin_En, true);
     gpio_put(Pin_CP, true);
-    gpio_put(Pin_Oe,true);
 
     // Variables locales
     unsigned int data = 0x0001;
     unsigned int msk;
     char i;
     char j;
-    unsigned int tabDigit[4] = {0, 0, 0, 0};
 
-    spi_inst_t *spi = spi0;
+
+
     // Initialize SPI port at 1 MHz
     spi_init(spi, 50000);
     // Set SPI format
@@ -113,9 +263,11 @@ int main()
 
 
     std::string str;
-    sleep_ms(2000);
+
     gpio_put(Pin_STR, true); //Select this device
+    RegWrite(spi,Pin_CS,addCTRL,0x04);
     // Boucle infinie
+
     while (true)
     {
 //        // Boucle sur les chiffres
